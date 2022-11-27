2ea0d19bfd6440b30d6a5c943b74bb34eea376e4
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-27 22:45:46 +0100

    add date

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
 setInterval(() => {
   const time = new Date();
   const month = time.getMonth();
@@ -15,12 +39,9 @@ setInterval(() => {
   const minutes = time.getMinutes();
   const ampm = hour >= 12 ? 'PM' : 'AM';
 
-  timeEl.innerHTML =
-    hoursIn12HrFormat +
-    ':' +
-    minutes +
-    '' +
-    `
- <span id="am-pm">${ampm}</span>
+  timeEl.innerHTML = `
+    ${hoursIn12HrFormat}: ${minutes}<span id="am-pm">${ampm}</span>
  `;
+  dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month];
+
 }, 1000);
